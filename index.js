c1b87e9375bb3959eb94cfa09e189938bdc117b1
Milennys <milennys.carrion@u4innovation.com>
2020-09-29 12:12:51 -0400

    update index

--- index.js
+++ index.js
@@ -1,30 +1,60 @@
 require('dotenv').config();
+const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
 
 const { dbConnection } = require('./database/config');
-//Creando el server de express
+
+
+// Crear el servidor de express
 const app = express();
 
+// Configurar CORS
 app.use(cors());
-//Lectura y parse de body
+
+// Lectura y parseo del body
 app.use(express.json());
 
+// Base de datos
 dbConnection();
 
-//Directorio Publico
+// Directorio público
 app.use(express.static('public'));
-//KGyymQczTKZ4nfgZ
-//dbMile
-//Rutas
-app.use('/api/usuarios', require('./routes/usuarios'))
-app.use( '/api/hospitales', require('./routes/hospitales') );
-app.use( '/api/medicos', require('./routes/medicos') );
-app.use( '/api/todo', require('./routes/busquedas') );
-app.use('/api/login', require('./routes/auth'))
-app.use( '/api/upload', require('./routes/uploads') );
 
+// Rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/hospitales', require('./routes/hospitales'));
+app.use('/api/medicos', require('./routes/medicos'));
+app.use('/api/todo', require('./routes/busquedas'));
+app.use('/api/login', require('./routes/auth'));
+app.use('/api/upload', require('./routes/uploads'));
+
+// Lo último
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+
+});
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.listen(process.env.PORT, () => {
-    console.log('Server' + process.env.PORT)
-})
+    console.log('Servidor corriendo en puerto ' + process.env.PORT);
+});
+
